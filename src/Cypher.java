349b80bb7521653629287228799f94615c7399c0
Bokurai <mrnere88@gmail.com>
2024-04-04 18:42:58 +0200

    Cypher methods class

---
+++ src/Cypher.java
@@ -0,0 +1,19 @@
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import java.security.NoSuchAlgorithmException;
+
+public class Cypher {
+    public static SecretKey keygenKeyGeneration(int keySize) {
+        SecretKey sKey = null;
+        if ((keySize == 128)||(keySize == 192)||(keySize == 256)) {
+            try {
+                KeyGenerator kgen = KeyGenerator.getInstance("AES");
+                kgen.init(keySize);
+                sKey = kgen.generateKey();
+            } catch (NoSuchAlgorithmException ex) {
+                System.err.println("Generador no disponible.");
+            }
+        }
+        return sKey;
+    }
+}
